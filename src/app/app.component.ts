36626651c157c2e75e83f1157d76544767a2e946
PabloG02 <tioo23000@gmail.com>
2024-02-02 22:18:26 +0100

    Set page main layout and set initial routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LandingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'dockerfiles-website';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { SearchListComponent } from './search-list/search-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Bioinformatics Docker Images Project',
+        component: LandingComponent,
+    },
+    {
+        path: 'containers',
+        component: SearchListComponent,
+    },
+];
