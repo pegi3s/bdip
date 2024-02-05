8b7555c292acc950a906f5e58e5b978b91add688
PabloG02 <tioo23000@gmail.com>
2024-02-05 15:34:19 +0100

    WIP Container list

--- src/app/search-list/search-list.component.ts
+++ src/app/search-list/search-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from "@angular/core";
 import dockerfiles from '../../assets/contents.json';
 
 @Component({
@@ -8,8 +8,8 @@ import dockerfiles from '../../assets/contents.json';
   templateUrl: './search-list.component.html',
   styleUrl: './search-list.component.css'
 })
-export class SearchListComponent {
-  containers = dockerfiles;
+export class SearchListComponent implements AfterViewInit {
+  //containers = dockerfiles;
 
   /*constructor() {
     dockerfiles.forEach((element) => {
@@ -18,4 +18,45 @@ export class SearchListComponent {
       }
     });
   }*/
+
+  containers = new Map<string, Dockerfile[]>();
+  constructor() {
+    dockerfiles.forEach((element) => {
+      if (!this.containers.has(element.category)) {
+        this.containers.set(element.category, [element])
+      } else {
+        this.containers.get(element.category)?.push(element);
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    const headings = Array.from(document.querySelectorAll('.categories + div > h2'));
+    const tocAnchors = Array.from(document.querySelectorAll('.categories > a'));
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const index = headings.indexOf(entry.target as HTMLHeadingElement);
+            tocAnchors.forEach((tab) => tab.classList.remove("active"));
+            if (index !== -1) {
+              tocAnchors[index].classList.add("active");
+            }
+          }
+        });
+      },
+      { threshold: 1.0 }
+    );
+
+    headings.forEach((hTwo) => observer.observe(hTwo));
+  }
+}
+
+interface Dockerfile {
+  category: string;
+  name: string;
+  description: string;
+  docs: string;
+  // include other properties as needed
 }
