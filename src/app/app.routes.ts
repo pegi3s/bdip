bf03bb36cabcdbabe70e1e168b7367224489d3ca
PabloG02 <tioo23000@gmail.com>
2024-02-25 21:33:58 +0100

    Show header fullscreen in the root route and compact in the rest of routes
    The introduction of an empty component was needed as it is required to specify a component for each route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { SearchListComponent } from './search-list/search-list.component';
+import { EmptyComponent } from './empty/empty.component';
 
 export const routes: Routes = [
     {
         path: '',
+        title: 'Bioinformatics Docker Images Project',
+        component: EmptyComponent,
+    },
+    {
+        path: 'about',
         title: 'Bioinformatics Docker Images Project',
         component: LandingComponent,
     },
---
+++ src/app/empty/empty.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty',
+  standalone: true,
+  imports: [],
+  template: '',
+  styleUrl: './empty.component.css'
+})
+export class EmptyComponent {
+
+}
