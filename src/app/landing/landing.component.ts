5e233829fd00c2ca189d231341fddc9c694e9879
PabloG02 <tioo23000@gmail.com>
2024-02-25 21:12:51 +0100

    Add whale image to About project section and add a new section How to start

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ContributorListComponent } from '../contributor-list/contributor-list.component';
 import { SearchComponent } from '../search/search.component';
+import { SearchGuidedComponent } from "../search-guided/search-guided.component";
 import { SearchListComponent } from '../search-list/search-list.component';
 import { ReasonListComponent } from '../reason-list/reason-list.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [ReasonListComponent, ContributorListComponent, SearchComponent, SearchListComponent],
+  imports: [ReasonListComponent, ContributorListComponent, SearchComponent, SearchGuidedComponent, SearchListComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
 })
