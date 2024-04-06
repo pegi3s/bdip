c350c26c1ef9dc9bc1e772cee6666f18090b24ad
PabloG02 <tioo23000@gmail.com>
2024-04-06 21:45:45 +0200

    Populate search by category section with retrieved data from service

--- src/app/search-guided/search-guided.component.ts
+++ src/app/search-guided/search-guided.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { delay } from 'rxjs';
+import { ContainerService } from '../services/container.service';
+import { TermStanza } from '../obo/TermStanza';
 
 @Component({
   selector: 'app-search-guided',
   standalone: true,
   imports: [],
   templateUrl: './search-guided.component.html',
-  styleUrl: './search-guided.component.css'
+  styleUrl: './search-guided.component.css',
 })
 export class SearchGuidedComponent {
-  categories = ["General tools", "Image building tools", "Recombination", "Plasmid prediction"];
+  containerService: ContainerService = inject(ContainerService);
+
+  rootCategories: TermStanza[] = [];
+  categories: TermStanza[] = [];
+
+  ngOnInit() {
+    this.containerService.getOntology().subscribe((ontology) => {
+      const roots = ontology
+        .getOntology()
+        ?.filter((term) => term.hasParents() === false);
+      roots?.forEach((root) => {
+        this.rootCategories.push(root);
+        this.categories.push(root);
+      });
+    });
+  }
+
+  loadChildren(category: TermStanza): void {
+    if (!category.hasChildren()) {
+      return;
+    }
+    this.categories = category.getChildren();
+  }
+
+  goUpLevel(): void {
+    const parent = this.categories[0].getParents()[0];
+    if (parent === undefined || !parent.hasParents()) {
+      this.categories = this.rootCategories;
+    } else {
+      this.categories = parent.getParents()[0].getChildren();
+    }
+  }
 }
