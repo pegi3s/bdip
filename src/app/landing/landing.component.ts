9e92d1d7a2edbd4e4d92ad8dc3ff6eaf786e8109
PabloG02 <tioo23000@gmail.com>
2024-04-06 21:41:15 +0200

    Add intro section to the landing page and improve layout on large displays

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core';
-import { ContributorListComponent } from '../contributor-list/contributor-list.component';
+import { Component, inject } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { SearchComponent } from '../search/search.component';
 import { SearchGuidedComponent } from "../search-guided/search-guided.component";
 import { SearchListComponent } from '../search-list/search-list.component';
 import { ReasonListComponent } from '../reason-list/reason-list.component';
+import { CarouselComponent } from '../carousel/carousel.component';
+import { ContributorCardComponent } from "../contributor-card/contributor-card.component";
+import { ContributorService } from '../services/contributor.service';
+import { Contributor } from '../models/contributor.model';
 
 @Component({
-  selector: 'app-landing',
-  standalone: true,
-  imports: [ReasonListComponent, ContributorListComponent, SearchComponent, SearchGuidedComponent, SearchListComponent],
-  templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css',
+    selector: 'app-landing',
+    standalone: true,
+    templateUrl: './landing.component.html',
+    styleUrl: './landing.component.css',
+    imports: [CarouselComponent, ReasonListComponent, SearchComponent, SearchGuidedComponent, SearchListComponent, NgOptimizedImage, ContributorCardComponent]
 })
 export class LandingComponent {
+  contributorService: ContributorService = inject(ContributorService);
 
+  contributors: Contributor[];
+  supporters: string[] = [
+    'assets/images/logo-cresc_algarve_2020.png',
+    'assets/images/logo-lisboa_2020.webp',
+    'assets/images/logo-norte_2020.png',
+    'assets/images/logo-portugal_2020.png',
+    'assets/images/logo-uniao_europeia_fundos_europeus.png',
+    'assets/images/logo-fct.png',
+  ];
+
+  constructor() {
+    this.contributors = this.contributorService.getContributors();
+  }
 }
