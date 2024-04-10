4a170e09f5943961604cac921a26009340cc2913
PabloG02 <tioo23000@gmail.com>
2024-04-11 00:15:26 +0200

    Add tooltips for organizations in contributor card to display their full name

--- src/app/services/contributor.service.ts
+++ src/app/services/contributor.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ContactType } from '../models/contact-type';
 import { Contributor } from '../models/contributor.model';
-import { Organization } from '../models/organization';
+import * as Organization from '../models/organization';
 
 @Injectable({
   providedIn: 'root'
