6854cf9f2fc2362cb35941bf6ad83fbc4cdb5371
PabloG02 <tioo23000@gmail.com>
2024-06-01 22:11:36 +0200

    Allow clipboard button to receive as input the element from where the text will be copied

--- src/app/shared/components/clipboard-button/clipboard-button.component.ts
+++ src/app/shared/components/clipboard-button/clipboard-button.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, input } from '@angular/core';
+import ClipboardJS from 'clipboard';
 
 @Component({
   selector: 'app-clipboard-button',
@@ -8,9 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './clipboard-button.component.css'
 })
 export class ClipboardButtonComponent {
+  element = input<ElementRef<any>>();
   copied: boolean = false;
 
   onCopyToClipboard() {
+    if (this.element()) {
+      ClipboardJS.copy(this.element()?.nativeElement);
+    }
+
     this.copied = true;
     setTimeout(() => {
       this.copied = false;
