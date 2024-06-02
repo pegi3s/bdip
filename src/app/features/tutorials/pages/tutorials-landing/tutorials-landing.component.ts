2cbb6046de27e0ff322ce38002fbb889adeb11b0
PabloG02 <tioo23000@gmail.com>
2024-06-02 18:08:14 +0200

    Tweak styles

--- src/app/features/tutorials/pages/tutorials-landing/tutorials-landing.component.ts
+++ src/app/features/tutorials/pages/tutorials-landing/tutorials-landing.component.ts
@@ -2,15 +2,20 @@ import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TutorialService } from '../../../../services/tutorial.service';
 import { Tutorial } from '../../../../models/tutorial';
+import { ThemeService } from '../../../../services/theme.service';
 
 @Component({
   selector: 'app-tutorials-landing',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './tutorials-landing.component.html',
-  styleUrl: './tutorials-landing.component.css'
+  styleUrl: './tutorials-landing.component.css',
+  host: {'[class.dark]':'isDarkTheme'}
 })
 export class TutorialsLandingComponent {
+  themeService: ThemeService = inject(ThemeService);
+  isDarkTheme: boolean = false;
+
   tutorialService: TutorialService = inject(TutorialService);
   tutorials = signal<Tutorial[]>([]);
 
@@ -18,5 +23,8 @@ export class TutorialsLandingComponent {
     this.tutorialService.getTutorials().subscribe(tutorials => {
       this.tutorials.set(tutorials);
     });
+    this.themeService.isDarkTheme().subscribe(
+      isDark => this.isDarkTheme = isDark
+    );
   }
 }
