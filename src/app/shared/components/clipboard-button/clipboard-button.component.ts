84dc545aa1a2be443ec35899b58751b03e532007
PabloG02 <tioo23000@gmail.com>
2024-06-22 10:57:24 +0200

    Clear selection after copying element with ClipboardButton

--- src/app/shared/components/clipboard-button/clipboard-button.component.ts
+++ src/app/shared/components/clipboard-button/clipboard-button.component.ts
@@ -9,13 +9,21 @@ import ClipboardJS from 'clipboard';
   styleUrl: './clipboard-button.component.css'
 })
 export class ClipboardButtonComponent {
+  /* Input */
   element = input<ElementRef<any>>();
   text = input<string>();
+
+  /* State */
   copied: boolean = false;
 
+  /**
+   * Copies the content of the element or text to the clipboard.
+   * After copying, it sets a `copied` flag to true for 1 second to indicate success.
+   */
   onCopyToClipboard() {
     if (this.element()) {
       ClipboardJS.copy(this.element()?.nativeElement);
+      document.getSelection()?.removeAllRanges();
     } else if (this.text()) {
       ClipboardJS.copy(this.text()!);
     }
